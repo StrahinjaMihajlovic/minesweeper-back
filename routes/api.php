e4c174f1a5b7b3dadc07f0a6c68ebd85c7d12b0a
Strahinja <strahinja.mihajlovic@quantox.com>
2021-06-03 10:03:35 +0200

    Added api routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+
+    'middleware' => 'api',
+
+], function ($router) {
+
+    Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('test', [\App\Http\Controllers\AuthController::class, 'test']);
+    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+
 });
+
+
+
